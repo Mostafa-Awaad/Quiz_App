28b99cc03d8549ac2748c962a0773f583eaf50e9
Mostafa-Awaad <mostafamohamedkamal2000@gmail.com>
2024-12-24 12:34:37 +0200

    Add model for the questions and dummy data for questions

---
+++ lib/models/quiz_question.dart
@@ -0,0 +1,6 @@
+class QuizQuestion {
+  const QuizQuestion(
+      {required this.questionText, required this.questionAnswers});
+  final String questionText;
+  final List<String> questionAnswers;
+}
