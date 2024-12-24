f48ebb23f4f116c11926e2871aa48fe6c02681ac
Mostafa-Awaad <mostafamohamedkamal2000@gmail.com>
2024-12-24 15:01:08 +0200

    format

--- lib/models/quiz_question.dart
+++ lib/models/quiz_question.dart
@@ -1,6 +1,8 @@
 class QuizQuestion {
-  const QuizQuestion(
-      {required this.questionText, required this.questionAnswers});
+  const QuizQuestion({
+    required this.questionText,
+    required this.questionAnswers,
+  });
   final String questionText;
   final List<String> questionAnswers;
 }
