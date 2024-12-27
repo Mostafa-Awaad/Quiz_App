79fcd8fca340f8ded4c25c2b1e8bd4b9a9b124da
Mostafa-Awaad <mostafamohamedkamal2000@gmail.com>
2024-12-27 08:08:59 +0200

    Adding styling to question text + Navigating to next question by indexing

--- lib/answer_button.dart
+++ lib/answer_button.dart
@@ -13,16 +13,16 @@ class AnswerButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return ElevatedButton(
       style: ElevatedButton.styleFrom(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 40,
-          vertical: 10
-        ),
+        padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 10),
         foregroundColor: Colors.white,
         backgroundColor: const Color.fromARGB(255, 103, 29, 116),
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
       ),
       onPressed: onTap,
-      child: Text(buttonText),
+      child: Text(
+        buttonText,
+        textAlign: TextAlign.center,
+      ),
     );
   }
 }
--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/answer_button.dart';
 import 'package:quiz_app/data/questions.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -12,9 +13,18 @@ class QuestionsScreen extends StatefulWidget {
 }
 
 class _QuestionScreenState extends State<QuestionsScreen> {
+  // Defining a variable for current question index
+  var currentIndex = 0;
+  //Adding method for incrementing currentIndex of answerQuestion
+  void answerQuestion() {
+    setState(() {
+      currentIndex++;
+    });
+  }
+
   @override
   Widget build(context) {
-    var currentQuestion = questions[0];
+    var currentQuestion = questions[currentIndex];
     return Container(
       padding: const EdgeInsets.all(40),
       decoration: const BoxDecoration(
@@ -32,9 +42,10 @@ class _QuestionScreenState extends State<QuestionsScreen> {
             children: [
               Text(
                 currentQuestion.questionText,
-                style: const TextStyle(
-                  fontSize: 16,
+                style: GoogleFonts.ptSerif(
                   color: Colors.white,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
                 ),
                 textAlign: TextAlign.center,
               ),
@@ -42,7 +53,7 @@ class _QuestionScreenState extends State<QuestionsScreen> {
               // Mapping list of the questionAnswers to a list of AnswerButton
               // Mapping does not change in the list content, it yields another copy
               ...currentQuestion.getShuffled().map((answer) {
-                return (AnswerButton(buttonText: answer, onTap: () {}));
+                return (AnswerButton(buttonText: answer, onTap: answerQuestion));
               }),
             ],
           ),
